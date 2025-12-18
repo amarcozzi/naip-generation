419701588b13107c0beea1ffdc2464d3d7d9522c
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-12-18 09:20:04 -0700

    Add submit scripts

--- scripts/submit_generate_coconino_chunk.sh
+++ scripts/submit_generate_coconino_chunk.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --account=umontana_fire_modeling
 #SBATCH --time=12:00:00
-#SBATCH --job-name=coconino_nf_data
+#SBATCH --job-name=coconino_chunk_data
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --output=log_generate_coconino_nf.out
+#SBATCH --output=log_generate_coconino_chunk.out
 
 source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
 conda activate naip-gen
 
 python generate.py \
-    --roi-file polygons/coconino_nf.geojson \
+    --roi-file polygons/coconino_chunk.geojson \
     --gsd 0.3 \
-    --train 250000 \
-    --test 50000 \
+    --train 10000 \
+    --test 1000 \
     --val 0 \
     --png \
-    coconino_nf
+    coconino_chunk
--- scripts/submit_generate_coconino_nf.sh
+++ scripts/submit_generate_coconino_nf.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --account=umontana_fire_modeling
 #SBATCH --time=12:00:00
-#SBATCH --job-name=coconino_chunk_data
+#SBATCH --job-name=coconino_nf_data
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --output=log_generate_coconino_chunk.out
+#SBATCH --output=log_generate_coconino_nf.out
 
 source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
 conda activate naip-gen
 
 python generate.py \
-    --roi-file polygons/coconino_chunk.geojson \
+    --roi-file polygons/coconino_nf.geojson \
     --gsd 0.3 \
-    --train 10000 \
-    --test 1000 \
+    --train 250000 \
+    --test 50000 \
     --val 0 \
     --png \
-    coconino_chunk
+    coconino_nf
