4bdce60dab1e488e4f389967444a2db9e78e928f
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-12-18 09:04:01 -0700

    Add submit scripts

---
+++ submit_train_model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=umontana_fire_modeling
+#SBATCH --partition=gpu-a100
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name="gen_naip"
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
+#SBATCH --time=2-0
+#SBATCH --output=log_model_train.out
+
+module load cuda
+
+source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
+conda activate canopy-flow
+
+python model.py \
+    --dataset_name coconino_nf
